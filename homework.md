e92f416fe7ee2f1d7f0ee7859f796dcaae0ccec9
Srushti Daware <srushtid7@gmail.com>
2025-07-02 11:23:23 -0400

    exploring GET,POST,DELETE,PUT,PATCH http handlers

---
+++ homework.md
@@ -0,0 +1,22 @@
+1. Create a local repository
+2. Initialize the repo -> npm init
+3. node_modules,package.json,package-lock.json
+4. Install express -> npm i express
+5. Create a server
+6. Listen to port 7777
+7. Write request handlers for /test , /hello
+8. Install nodemon and update scripts inside package.json
+9. What are dependencies
+10. What is the use of "-g" while npm install
+11. Difference between ^ and ~ in package versions
+
+-initialize git - git init -> git add . -> git commit -m "your message"
+git remote add origin git@github.com:srushtid7/devTinder.git
+git branch -M main
+git push -u origin main
+
+-.gitignore
+-Create a remote repo on github
+-Push all code to remote origin
+-play with order routes like /hello,/hello/2,/xyz
+-order of the routes is very important
--- src/app.js
+++ src/app.js
@@ -3,12 +3,52 @@ const express = require("express");
 const app = express(); //instance of express js application
 
 //request handler function takes route(optional) and CB as parameter
-app.use("/test",(req,res)=>{
-res.send("Namaste!!! from the test server!");
-})
-app.use("/hello",(req,res)=>{
-   res.send("Hello hello from server hello!");
-})
-app.listen(7777,()=>{ //takes port and callback function
-    console.log("Server is successfully running on 7777!")
+// app.use("/user",(req,res)=>{
+//   res.send("HAHAHAHAH");
+// }); // this will get priority as the order matters
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Srushti", lastName: "Bhoyar" });
+}); //this will only handle GET call to /user
+
+app.post("/user",(req,res)=>{
+  //saving data to DB
+  res.send("Data saved to DB!");
+});
+
+app.put("/user",(req,res)=>{
+  //saving data to DB
+  res.send("Data PUT to DB!");
+});
+
+app.patch("/user",(req,res)=>{
+  //saving data to DB
+  res.send("Data pacthed to DB!");
+});
+
+app.delete("/user",(req,res)=>{
+  res.send("Deleted successfully");
+});
+
+// .use will match all the HTTP method API calls to /test
+app.use("/test", (req, res) => {
+  res.send("Namaste!!! from the test!");
+});
+
+//the below order of routes is very important. /hello,/hello/2,/xyz
+// app.use("/hello/2",(req,res)=>{
+// res.send("HELLO 2");
+// });
+
+// app.use("/hello", (req, res) => {
+//   res.send("Hello hello from server hello!");
+// });
+
+// app.use("/", (req, res) => {
+//   res.send("Namaste!!! Srushti!");
+// });
+
+app.listen(7777, () => {
+  //takes port and callback function
+  console.log("Server is successfully running on 7777!");
 });
