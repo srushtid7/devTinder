7672f51d2f8fb3cdb2b233ae5756beebba0643a8
Srushti Daware <srushtid7@gmail.com>
2025-07-09 05:12:42 -0400

    route handlers demo and next()

--- src/app.js
+++ src/app.js
@@ -3,50 +3,33 @@ const express = require("express");
 const app = express(); //instance of express js application
 
 //request handler function takes route(optional) and CB as parameter
-// app.use("/user",(req,res)=>{
-//   res.send("HAHAHAHAH");
-// }); // this will get priority as the order matters
-
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Srushti", lastName: "Bhoyar" });
-}); //this will only handle GET call to /user
-
-app.post("/user",(req,res)=>{
-  //saving data to DB
-  res.send("Data saved to DB!");
-});
-
-app.put("/user",(req,res)=>{
-  //saving data to DB
-  res.send("Data PUT to DB!");
-});
-
-app.patch("/user",(req,res)=>{
-  //saving data to DB
-  res.send("Data pacthed to DB!");
-});
-
-app.delete("/user",(req,res)=>{
-  res.send("Deleted successfully");
-});
 
 // .use will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-  res.send("Namaste!!! from the test!");
+app.use("/user", (req, res,next) => {
+  console.log("handling first route handler");
+   next();
+  
+},
+[(req,res,next)=>{
+  console.log("handling second route");
+  //res.send("response 2!");
+  next();
+},
+(req,res,next)=>{
+  console.log("handling third route");
+  //res.send("response 3!");
+  next();
+}],
+(req,res,next)=>{
+  console.log("handling fourth route");
+ // res.send("response 4!");
+  next();
+},
+(req,res,next)=>{
+  console.log("handling fifth route");
+ res.send("response 5!");
 });
 
-//the below order of routes is very important. /hello,/hello/2,/xyz
-// app.use("/hello/2",(req,res)=>{
-// res.send("HELLO 2");
-// });
-
-// app.use("/hello", (req, res) => {
-//   res.send("Hello hello from server hello!");
-// });
-
-// app.use("/", (req, res) => {
-//   res.send("Namaste!!! Srushti!");
-// });
 
 app.listen(7777, () => {
   //takes port and callback function
