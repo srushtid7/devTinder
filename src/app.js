10899a90e998916b6289aa2f5600d8b54bd208ee
Srushti Daware <srushtid7@gmail.com>
2025-07-10 05:09:09 -0400

    global error handler

--- src/app.js
+++ src/app.js
@@ -8,13 +8,15 @@ app.get("/user", userAuth, (req, res) => {
   res.send("User data sent!"); //admin middleware will not count for /user
 });
 app.get("/admin/getAllData", (req, res) => {
+  throw new Error("djshfg");
   res.send("All data sent!");
 });
 
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("User Deleted!");
-});
-
+app.use("/",(err,req,res,next)=>{
+  if(err){
+    res.status(500).send("something went wrong");
+  }
+})
 app.listen(7777, () => {
   //takes port and callback function
   console.log("Server is successfully running on 7777!");
