d8b4cb6cb91cfcbd28241261373fa865cbbb156e
Srushti Daware <srushtid7@gmail.com>
2025-07-16 00:49:07 -0400

    schema level validations

--- src/app.js
+++ src/app.js
@@ -5,17 +5,90 @@ const User = require("./models/user");
 
 app.use(express.json()); // this middleware converts all the req into json
 
+//Add new user to DB
 app.post("/signup", async (req, res) => {
-
-  console.log(req.body);
+  console.log(req.body); //check in postman
   try {
-    const user = new User(req.body);  //new instance of User Model which is imported from models
+    const user = new User(req.body); //new instance of User Model which is imported from models
     await user.save();
-    res.send("User Added Successfully!");
+    res.send("User Added Successfully to DB!");
   } catch (err) {
     res.status(400).send("Error saving the user:" + err.message);
   }
 });
+
+//Get user to UI/Postman from DB by emailId
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  console.log("User EmailID received:", userEmail);
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found!");
+    } else {
+      res.send(users); //getting response in postman after adding emailId in body-json section
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+// Get all the usrs
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("User not found!");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// delete th user by _id. use http://localhost:7777/user?userId=687109084461b079e58c85c7 in postman to execute
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    await User.findByIdAndDelete({ _id: userId });
+    res.send("User is deleted sucessfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//update data of the user using _id
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  console.log(userId);
+  const data = req.body;
+  console.log(data);
+  try {
+    await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong " + err.message);
+  }
+});
+
+/**app.patch("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  console.log(userEmail);
+  const userData = { ...req.body };
+  console.log(userData);
+  delete userData._id;
+  try {
+    await User.findOneAndUpdate({ emailId: userEmail }, userData);
+    res.send("User updated successfully via emailId");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});**/
 
 connectDB()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,27 +1,57 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    firstName:{
-        type:String
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
     },
-    lastName:{
-        type:String
+    lastName: {
+      type: String,
     },
-    emailId:{
-        type:String
+    emailId: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
     },
-    password:{
-        type:String
+    password: {
+      type: String,
+      required: true,
     },
-    age:{
-        type:Number
+    age: {
+      type: Number,
+      min: 18,
     },
-    gender:{
-        type:String
+    gender: {
+      type: String,
+      lowercase: true,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid!");
+        }
+      },
     },
-    city:{
-        type:String
-    }
-});
+    city: {
+      type: String,
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://img.freepik.com/premium-vector/person-with-blue-shirt-that-says-name-person_1029948-7040.jpg",
+    },
+    about: {
+      type: String,
+      default: "This is default information section about the user!",
+    },
+    skills: {
+      type: [String],
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
